32e3eec4239f834d66b67a7e1039a08940b22a43
Бояршинов Константин <costya74@bk.ru>
2022-02-06 10:56:31 +0500

    New added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-       int balance = 100;
-       int payment = 100; // сумма единаличного платежа
-       int bonus = 1; // за каждые внесенные 100 рублей
-       int limit =100; // минимальная сумма на которую начисляются бонусы
-
-       int amountBonus;
-       if (payment >= 1000) {
-           amountBonus = payment * bonus / limit;
-       }
-       else {
-           amountBonus = 0;
-       }
-       int total = balance + payment + amountBonus;
+        int balance = 100; // остаток на счету
+        int payment = 100; // сумма единаличного платежа
+        int bonus = 1; // за каждые внесенные 100 рублей
+        int limit = 100; // минимальная сумма на которую начисляются бонусы
+        int amountBonus;
+        if (payment >= 1000) {
+            amountBonus = payment * bonus / limit;
+        } else {
+            amountBonus = 0;
+        }
+        int total = balance + payment + amountBonus;
         System.out.println(" Баланс счета составляет " + total + " Из них бонусов " + amountBonus);
     }
 }
